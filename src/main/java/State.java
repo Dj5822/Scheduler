da164656894a56e0a54b6813bb31f6296bd160c6
jlob275 <68893201+jlob275@users.noreply.github.com>
2021-08-08 15:32:12 +1200

    New Schedule class
    stores whole schedule on each node, increasing memory usage but likely improving performance

--- src/main/java/State.java
+++ src/main/java/State.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Represents a possible scheduling State for a single Task.
  **/
 public class State {
-    private Task task;
-    private int startTime;
-    private int processor;
+    protected Task task;
+    protected int startTime;
+    protected int processor;
 
     public State(Task task, int startTime, int processor) {
         this.task = task;
         this.startTime = startTime;
         this.processor = processor;
+    }
+
+    public State(Task task) {
+        this.task = task;
+        this.startTime = 0;
+        this.processor = 0;
     }
 
     public int getStartTime() {
@@ -36,4 +45,99 @@ public class State {
         System.out.println();
     }
 
+}
+
+class Schedule extends State {
+    private HashMap<Task,State> scheduled;
+    private ArrayList<Task> schedulable;
+    private int[] processorFinishTimes;
+
+    /**
+     * Constructor for a schedule derived from a parent schedule
+     * @param task most recently scheduled task
+     * @param startTime start time of parameter task
+     * @param processor processor assigned to parameter task
+     * @param parentSchedule parent schedule to derive from
+     */
+    public Schedule(Task task, int startTime, int processor, Schedule parentSchedule) {
+        super(task, startTime, processor);
+
+        scheduled = new HashMap<Task,State>(parentSchedule.getScheduledTasks());
+        scheduled.put(task, this);
+
+        schedulable = new ArrayList<Task>(parentSchedule.getSchedulableTasks());
+        schedulable.remove(task);
+
+        processorFinishTimes = parentSchedule.getProcessorFinishTimes().clone();
+        processorFinishTimes[processor] = processorFinishTimes[getFinishTime()];
+
+        // mark children whose parents are all scheduled for scheduling
+        for (Edge childEdge : task.getChildren()) {
+            Task child = childEdge.getChild();
+            boolean allParentsScheduled = true;
+            for  (Task parentTask : child.getParents()) {
+                if (!taskisScheduled(parentTask)) {
+                    allParentsScheduled = false;
+                    break;
+                }
+            }
+            if (allParentsScheduled) {
+                schedulable.add(child);
+            }
+        }
+    }
+
+    /**
+     * Constructor for a start task schedule
+     * @param task start task
+     * @param processorCount number of processors to schedule for
+     * @param startTasks array of every start task
+     */
+    public Schedule(Task task, short processorCount, ArrayList<Task> startTasks) {
+        super(task);
+
+        scheduled = new HashMap<Task,State>();
+        scheduled.put(task, this);
+
+        schedulable = new ArrayList<Task>(startTasks);
+        schedulable.remove(task);
+        
+        processorFinishTimes = new int[processorCount];
+        processorFinishTimes[processor] = processorFinishTimes[getFinishTime()];
+
+        // mark children whose parents are all scheduled for scheduling
+        for (Edge childEdge : task.getChildren()) {
+            Task child = childEdge.getChild();
+            boolean allParentsScheduled = true;
+            for  (Task parentTask : child.getParents()) {
+                if (!taskisScheduled(parentTask)) {
+                    allParentsScheduled = false;
+                    break;
+                }
+            }
+            if (allParentsScheduled) {
+                schedulable.add(child);
+            }
+        }
+    }
+
+    public boolean taskisScheduled(Task task) {
+        return scheduled.containsKey(task);
+    }
+
+    public State getTaskState(Task task) {
+        return scheduled.get(task);
+    }
+
+    public HashMap<Task,State> getScheduledTasks() {
+        return scheduled;
+    }
+
+    public ArrayList<Task> getSchedulableTasks() {
+        return schedulable;
+    }
+
+    public int[] getProcessorFinishTimes() {
+        return processorFinishTimes;
+    }
 }
