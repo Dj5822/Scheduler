11bb497648c00c2a71555def11453a39126381ed
bpal938 <69737582+bpal938@users.noreply.github.com>
2021-08-20 14:22:20 +1200

    Adding hover info to gantt chart

--- src/main/java/GanttChart.java
+++ src/main/java/GanttChart.java
@@ -103,6 +103,9 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
                         ellipse.setWidth( getLength( item.getExtraValue()) * ((getXAxis() instanceof NumberAxis) ? Math.abs(((NumberAxis)getXAxis()).getScale()) : 1));
                         ellipse.setHeight(getBlockHeight() * ((getYAxis() instanceof NumberAxis) ? Math.abs(((NumberAxis)getYAxis()).getScale()) : 1));
                         y -= getBlockHeight() / 2.0;
+                        ellipse.hoverProperty().addListener((observable)->{
+                            
+                        });
 
                         // Note: workaround for RT-7689 - saw this in ProgressControlSkin
                         // The region doesn't update itself when the shape is mutated in place, so we
