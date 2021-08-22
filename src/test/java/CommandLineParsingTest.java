99de1ec0ca0b073dcf9df5b1c9634c28cd77bdea
Whan <wjun856@aucklanduni.ac.nz>
2021-08-22 21:18:50 +1200

    Added skeleton code for CommandLineParsingTest

---
+++ src/test/java/CommandLineParsingTest.java
@@ -0,0 +1,17 @@
+import com.paypal.digraph.parser.GraphNode;
+import com.paypal.digraph.parser.GraphEdge;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.apache.commons.io.FileUtils;
+import java.io.*;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The CommandLineParsingTest class contains tests to check functionality of command line
+ * argument parsing in App class
+ */
+public class CommandLineParsingTest {
+    
+}
